afc1e191e90cbb3d0e0e64444db89853ccd6579c
kausar017 <mdkousarmia71@gmail.com>
2025-10-03 10:26:28 +0600

    hero secion updated

--- src/Conponent/WebBody/Hero/Hero.jsx
+++ src/Conponent/WebBody/Hero/Hero.jsx
@@ -2,17 +2,19 @@ import { Typewriter } from "react-simple-typewriter";
 
 function Hero() {
   return (
-    <div className="hero bg-black text-white h-screen flex md:flex-col justify-around items-center ">
-      <div className="container flex justify-between items-center gap-10">
+    <div className="hero bg-black text-white flex md:flex-col justify-around items-center ">
+      <div className="container flex justify-between items-center gap-10 px-5 ">
         <div className="w-100% max-w-xl">
-          <p className="text-lg font-semibold">Hi I'm</p>
-          <h1 className="text-4xl font-bold mt-2">Kausar Mia</h1>
+          <p className="text-xl font-semibold">Hi I'm</p>
+          <h1 className="text-6xl font-bold mt-2">
+            <span className="text-[#ED2519]">Kausar</span> Mia
+          </h1>
           <h3 className="text-3xl font-semibold my-6">
             <Typewriter
               words={[
                 "Frontend Developer",
-                "Marn-stak Devoloper",
-                "CMS Devoloper",
+                "Mern-stack Developer",
+                "CMS Developer",
               ]}
               loop={true}
               cursor
@@ -29,17 +31,16 @@ function Hero() {
             on building responsive web applications.
           </p>
           <div className="flex gap-4">
-            <button className="btn btn-primary">Hire Me</button>
-            <button className="btn btn-secondary">Download CV</button>
+            <button className="btn bg-[#ED2519] border-none text-white">Hire Me</button>
+            <button className="btn text-white bg-transparent hover:bg-white/20">Download CV</button>
           </div>
         </div>
         <div>
           <img
-            src="https://res.cloudinary.com/dqjuyj19t/image/upload/v1758817632/0791f64f-95b5-40ca-97f5-5ac3eb526c4a_tfpkhu.png"
+            src="https://i.postimg.cc/QNBhzf6d/fg.png"
             alt="kausar"
-            className="w-full max-w-96 rounded-lg relative z-10"
+            className="w-full rounded-lg relative z-10"
           />
-            <div className="absolute w-72 h-72 bg-purple-700 rounded-full top-0 right-0 z-0 blur-3xl"></div>
         </div>
       </div>
     </div>
